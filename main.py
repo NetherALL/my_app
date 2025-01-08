d83f2ef458395c739f570ebc53532ac2a61689b6
11111 박현 <24_11111@daejin.sen.hs.kr>
2025-01-08 11:17:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ st.text('\n\n')
 st.write('안녕하세요 . 😒 집가고싶네요.')
 st.write('저의 이메일 주소는 24_11111@daejin.sen.hs.kr입니다')
 st.write('대진고등학교 학생이죠')
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
